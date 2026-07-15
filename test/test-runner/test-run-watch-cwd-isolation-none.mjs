f57776332c66d1c238dfa000859ec0555bfaa29c
Tim Perry <pimterry@gmail.com>
2026-07-15 16:08:25 +0200

    test: fix flaky watch + cwd + argv + no-isolation test-runner test
    Signed-off-by: Tim Perry <pimterry@gmail.com>

--- test/test-runner/test-run-watch-cwd-isolation-none.mjs
+++ test/test-runner/test-run-watch-cwd-isolation-none.mjs
@@ -1,12 +1,22 @@
 // Test run({ watch: true, cwd, isolation: 'none' }) runs with different cwd while in watch mode and isolation none
 import * as common from '../common/index.mjs';
+import assert from 'node:assert';
+import { writeFileSync } from 'node:fs';
+import { join } from 'node:path';
 import { run } from 'node:test';
 import tmpdir from '../common/tmpdir.js';
-import { refreshForTestRunnerWatch, skipIfNoWatch } from '../common/watch.js';
+import { skipIfNoWatch } from '../common/watch.js';
 
 skipIfNoWatch();
-refreshForTestRunnerWatch();
+tmpdir.refresh();
 
+writeFileSync(join(tmpdir.path, 'test.js'), `
+const test = require('node:test');
+
+test('test ran from cwd', () => {});
+`);
+
+const passed = [];
 const controller = new AbortController();
 const stream = run({
   cwd: tmpdir.path,
@@ -22,6 +32,8 @@ const stream = run({
 });
 
 stream.on('test:fail', common.mustNotCall());
-stream.on('test:pass', common.mustCall(1));
+stream.on('test:pass', common.mustCall((data) => passed.push(data.name), 1));
 // eslint-disable-next-line no-unused-vars
 for await (const _ of stream);
+
+assert.deepStrictEqual(passed, ['test ran from cwd']);
